6cbcabac1584649097171435ae2a0e2511d8aef2
«Aleksandr» <akhlopushin98@mail.ru>
2022-06-28 16:44:45 +0400

    добавили задачи про квадраты чисел

--- Задачи/Program.cs
+++ Задачи/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Возводит число в квадрат
+// Console.WriteLine("Введите целое число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int square = number * number;
+// Console.WriteLine($"Квадрат числа: {square}");
+
+// Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго
+Console.WriteLine("Введите целое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите 2 целое число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int square = number2*number2;
+if (number1 == square)
+{
+    Console.WriteLine($"Число {number1} является квадратом {number2}");
+}
+else
+{
+    Console.WriteLine($"Число {number1} не является квадратом {number2}");
+}
