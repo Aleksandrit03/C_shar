1e7b4dea9746040c680ee4e9136b43223f1c155d
«Aleksandr» <akhlopushin98@mail.ru>
2022-06-28 17:10:40 +0400

    добавили задачу про дни недели

--- Задачи/Program.cs
+++ Задачи/Program.cs
@@ -4,17 +4,61 @@
 // int square = number * number;
 // Console.WriteLine($"Квадрат числа: {square}");
 
-// Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго
-Console.WriteLine("Введите целое число: ");
-int number1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите 2 целое число: ");
-int number2 = Convert.ToInt32(Console.ReadLine());
-int square = number2*number2;
-if (number1 == square)
+// // Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго
+// Console.WriteLine("Введите целое число: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите 2 целое число: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// int square = number2*number2;
+// if (number1 == square)
+// {
+//     Console.WriteLine($"Число {number1} является квадратом {number2}");
+// }
+// else
+// {
+//     Console.WriteLine($"Число {number1} не является квадратом {number2}");
+// }
+
+
+Console.WriteLine("Введите число от 1 до 7: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+if (x ==1)
 {
-    Console.WriteLine($"Число {number1} является квадратом {number2}");
+    Console.WriteLine("Понедельник");
 }
-else
+
+if (x ==2)
 {
-    Console.WriteLine($"Число {number1} не является квадратом {number2}");
+    Console.WriteLine("Вторник");
+}
+
+if (x ==3)
+{
+    Console.WriteLine("Среда");
+}
+
+if (x ==4)
+{
+    Console.WriteLine("Четверг");
+}
+
+if (x ==5)
+{
+    Console.WriteLine("Пятница");
+}
+
+if (x ==6)
+{
+    Console.WriteLine("Суббота");
+}
+
+if (x ==7)
+{
+    Console.WriteLine("Воскресенье");
+}
+
+if (x > 7)
+{
+    Console.WriteLine("Данное число вне диапазона");
 }
