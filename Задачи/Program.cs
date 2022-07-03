27b8fdcf7651bd29a3993e472880921f2275611b
«Aleksandr» <akhlopushin98@mail.ru>
2022-07-03 16:48:43 +0400

    добавил задачи по 3 лекции

--- Задачи/Program.cs
+++ Задачи/Program.cs
@@ -63,3 +63,125 @@
 //     Console.WriteLine("Данное число вне диапазона");
 // }
 // Console.ReadKey();
+
+// Вывод сообщения некоторое кол-во раз
+// void Method1(string msg, int count)
+// {
+// int i = 0;
+// while (i < count)
+// {
+//     Console.WriteLine(msg);
+//     i++;
+// }
+// }
+// Method1("Tekst", 5); //Обращение к функции(методу)
+
+// Обращение к методу для вывода времени в консоль
+// int Method2()
+// {
+//     return DateTime.Now.Year;
+// }
+// int year = Method2();
+// Console.WriteLine(year);
+
+// Вывод последовательности несколько раз
+// string Method4(int count, string c)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+//     while(i < count)
+//     {
+//         result = result + c;
+//         i++;
+//     }
+//     return result;
+// }
+// string res = Method4(10, "СасоПавлиашвили");
+// Console.WriteLine(res);
+
+// Повторение предъидущей программы с циклом For
+// string Method4(int count, string c)
+// {
+//     string result = String.Empty;
+//     for (int i = 0; i < count; i++)
+//     {
+//         result = result + c;
+//     }
+//     return result;
+// }
+
+// string res = Method4(10, "СасоПавлиашвили");
+// Console.WriteLine(res);
+
+// Написание таблицы умножения
+// for (int i = 2; i <= 10; i++)
+// {
+//     for (int j = 2; j <= 10; j++)
+//      {
+//        Console.WriteLine($"{i} * {j} = {i * j}");
+//      }
+//      Console.WriteLine();
+// }
+
+// Работа с текстом
+// Дан текст. В тексте нужно все пробелы заменить черточками,
+// маленькие буквы "к" заменить большими "К",
+// а большие "С" заменить маленькими "с".
+// string text = "- Шел крокодил, трубку курил, трубка упала и написала, - Шишли мышли сопли вышли";
+// // string s = "qwerty"
+//          //    012345
+// // s[3] // r
+// string Replace(string text, char oldValue, char newValue)
+// {
+//     string result = String.Empty;
+
+//     int lenght = text.Length;
+//     for (int i =0; i < lenght; i++)
+//     {
+//         if(text[i] == oldValue) result = result + $"{newValue}"; 
+//         else result = result + $"{text[i]}";
+//     }
+
+//     return result;
+// }
+// string newText = Replace(text, ' ', '|');
+// Console.WriteLine(newText);
+// Console.WriteLine();
+// newText = Replace(newText, 'к', 'К');
+// Console.WriteLine(newText);
+// Console.WriteLine();
+// newText = Replace(newText, 'С', 'с');
+// Console.WriteLine(newText);
+// Console.WriteLine();
+
+
+// Перестановка чисел в массиве
+
+// int[] arr = { 1, 4, 8, 9, 1, 2, 4, 6, 3 };
+
+// void PrintArray(int[] array)
+// {
+//     int count = array.Length;
+//     for (int i = 0; i < count; i++)
+//     {
+//         Console.Write($"{array[i]}");
+//     }
+// }
+// void SelectionSort(int[] array)
+// {
+//     for (int i = 0; i < array.Length - 1; i++)
+//     {
+//         int minPosition = i;
+//         for (int j = i + 1; j < array.Length; j++)
+//         {
+//            if(array[j] < array[minPosition]) minPosition = j; 
+//         }
+//         int temporary = array[i];
+//         array[i] = array[minPosition];
+//         array[minPosition] = temporary;
+//     }
+// }
+// PrintArray(arr);
+// SelectionSort(arr);
+
+// PrintArray(arr);
