f4444ae2a469730f666d61536c5271c57090b3e5
«Aleksandr» <akhlopushin98@mail.ru>
2022-07-03 15:08:08 +0400

    D

--- Задачи/ЗадачиСеминар3/Задача19/Program.cs
+++ Задачи/ЗадачиСеминар3/Задача19/Program.cs
@@ -1,12 +1,12 @@
 ﻿// Вывод сообщения некоторое кол во раз
 
-void Method1(string msg, int count)
-{
-int i = 0;
-while (i < count)
-{
-    Console.WriteLine(msg);
-    i++;
-}
-}
-Method1("Tekst", 5); //Обращение к функции(методу)
+// void Method1(string msg, int count)
+// {
+// int i = 0;
+// while (i < count)
+// {
+//     Console.WriteLine(msg);
+//     i++;
+// }
+// }
+// Method1("Tekst", 5); //Обращение к функции(методу)
