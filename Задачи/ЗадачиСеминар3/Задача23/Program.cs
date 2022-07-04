b5bf0056dfe103ab23543c612a346d70f479f8f9
«Aleksandr» <akhlopushin98@mail.ru>
2022-07-04 19:43:14 +0400

    Создал папку с задачей 23 к 3 семинару

---
+++ Задачи/ЗадачиСеминар3/Задача23/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+namespace Zadacha23
+{
+   class Zadacha
+    {
+         static void Main(string[] args)
+        {
+          Console.WriteLine("Введите координаты первой точки");
+          
+         Console.ReadKey();
+         }
+     }  
+}   
