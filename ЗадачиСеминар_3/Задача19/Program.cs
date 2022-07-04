b228fdd747605f0335b8c01464e08c419987efab
«Aleksandr» <akhlopushin98@mail.ru>
2022-07-04 20:23:37 +0400

    create new folder 3 seminar

---
+++ ЗадачиСеминар_3/Задача19/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+// является ли оно палиндромом.
+
+namespace Zadacha19
+{
+   class Zadachi
+    {
+         static void Main(string[] args)
+        {
+          Console.WriteLine("Введите пятизначное число: ");
+          string? number = Console.ReadLine();
+          if (number.Length != 5)
+            {
+              Console.WriteLine($"Введенное число {number} не является пятизначным");
+            }
+          else if (number[0] == number[4] & number[1] == number[3])
+            {
+               Console.WriteLine($"Введенное число {number} является палиндромом");
+            } 
+        else 
+            {
+             Console.WriteLine($"Введенное число {number} не является палиндромом"); 
+            }
+         Console.ReadKey();
+         }
+     }  
+}   
---
+++ ЗадачиСеминар_3/Задача23/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+namespace Zadacha23
+{
+   class Zadacha
+    {
+         static void Main(string[] args)
+        {
+          Console.WriteLine("Введите число: ");
+          int N = Convert.ToInt32(Console.ReadLine());
+          int i = 0;
+          while(i <= N)
+          {
+            double result = Math.Pow(i, 3);
+            Console.WriteLine($"Куб числа {i} равен {result}");
+            i++;
+          }
+         Console.ReadKey();
+         }
+     }  
+}   
