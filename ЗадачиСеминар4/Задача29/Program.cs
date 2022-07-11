5c6a16f4ab07eca7e9fbb931ab8e40a9c6c09536
«Aleksandr» <akhlopushin98@mail.ru>
2022-07-11 20:22:42 +0400

    F

--- ЗадачиСеминар4/Задача29/Program.cs
+++ ЗадачиСеминар4/Задача29/Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая задает случайный массив случайного размера 
+// (от 5 до 10) элементов (значение элементов от 1 до 40) и выводит на экран 
+// массив квадратов этих чисел.
+namespace Zadacha29
+{
+   class Zadachi
+    {
+         static void Main(string[] args)
+        {
+          Console.WriteLine("Массив до возведения чисел в квадрат: ");
+          int [] array = new int [new Random().Next(5,10)];
+          for (int i = 0; i < array.Length; i++)
+          {
+            array [i] = Random.Next(1, 40);
+          }
+           for (int i = 0; i < array.Length; i++)
+          {
+            array [i] = Math.Pow(i, 2);
+          }
+          Console.WriteLine($"{array[i]}");
+         }
+     }  
+}   
+
