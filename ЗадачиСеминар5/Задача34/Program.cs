1a6397085a78178c0202b2dc7d71a6838fbbc594
«Aleksandr» <akhlopushin98@mail.ru>
2022-07-12 10:14:16 +0400

    Решена задача 34

--- ЗадачиСеминар5/Задача34/Program.cs
+++ ЗадачиСеминар5/Задача34/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+namespace Zadacha34
+{
+   class Zadachi
+    {
+         static void Main(string[] args)
+        {
+          int [] array = new int [new Random().Next(8, 10)];
+          Random random = new Random();
+          int result = 0;
+          int i = 0;
+          while (i < array.Length)
+          {
+            array [i] = random.Next(100, 1000);
+            Console.WriteLine($" {array[i]} ");
+            i++;
+          }
+          i = 0;
+          while (i < array.Length)
+          {
+            if (array[i] % 2 ==0)
+            result++;
+            i++;
+          }
+          Console.WriteLine($"Количество четных чисел: {result}");
+         }
+     }  
+}   
