9f31aaf7fcd552fc880a9b2f76fe106cd85e2abd
«Aleksandr» <akhlopushin98@mail.ru>
2022-07-12 10:28:52 +0400

    Решаем задачу 36

---
+++ ЗадачиСеминар5/Задача36/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+namespace Zadacha36
+{
+   class Zadachi
+    {
+         static void Main(string[] args)
+        {
+          int [] array = new int [new Random().Next(4, 4)];
+          Random random = new Random();
+          int result = 0;
+          int i = 0;
+          while (i < array.Length)
+          {
+            array [i] = random.Next(-10, 10);
+            Console.WriteLine($" {array[i]} ");
+            i++;
+          }
+          i = 1;
+          while (i < array.Length)
+          {
+            while (i % 2 ==0)
+            result = result + array[i];
+            i++;
+          }
+          Console.WriteLine($"Сумма чисел на нечетных позициях: {result}");
+         }
+     }  
+}   
