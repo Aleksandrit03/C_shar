6ffffddeed6e2afbaf66061cc65b799f30ef5803
«Aleksandr» <akhlopushin98@mail.ru>
2022-07-12 10:42:54 +0400

    Создана папка с задачей 38

---
+++ ЗадачиСеминар5/Задача38/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+namespace Zadacha36
+{
+   class Zadachi
+    {
+         static void Main(string[] args)
+        {
+          int [] array = new int [new Random().Next(4, 6)];
+          Random random = new Random();
+          int result = 0;
+          int i = 0;
+          while (i < array.Length)
+          {
+            array [i] = random.Next(-10, 10);
+            Console.WriteLine($" {array[i]} ");
+            i++;
+          }
+          i=1;
+          while (i < array.Length)
+          {
+            result = result + array[i];
+            i = i + 2;
+          }
+          Console.WriteLine($"Сумма чисел на нечетных позициях: {result}");
+          Console.ReadKey();
+         }
+     }  
+}   
+
