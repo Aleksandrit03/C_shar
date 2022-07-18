e6b745bd4d64025009544bcc0b35349cb5ab4eb5
«Aleksandr» <akhlopushin98@mail.ru>
2022-07-18 17:49:21 +0400

    Решена задача 43

--- ЗадачиСеминар6/Задача41/Program.cs
+++ ЗадачиСеминар6/Задача41/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача41. Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+namespace Zadacha41
+{
+   class Zadachi
+    {
+         static void Main(string[] args)
+        {
+             Console.WriteLine("Введите числа через пробел: ");
+             string? massiv = Console.ReadLine();
+             string [] numbers = massiv.Split(' ');
+             int i = 0;
+             int result = 0;
+             Console.WriteLine("Преобразованно в массив: ");
+             while (i < numbers.Length)
+             {
+                Console.Write($" {numbers[i]} ");
+                i++;
+                if (numbers [i] > 0) result++;
+             }
+         }
+     }  
+}   
