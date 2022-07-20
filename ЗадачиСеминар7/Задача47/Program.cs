49105429cd9470567959ee0e3adae6f7469cb53d
«Aleksandr» <akhlopushin98@mail.ru>
2022-07-20 13:10:17 +0400

    Create target47

--- ЗадачиСеминар7/Задача47/Program.cs
+++ ЗадачиСеминар7/Задача47/Program.cs
@@ -5,8 +5,28 @@ namespace Zadacha47
     {
          static void Main(string[] args)
         {
-             Console.WriteLine("Введите количество чисел, которое хотите ввести: ");
-             
+             Console.WriteLine("Введите значение строк m: ");
+             int m = Convert.ToInt32(Console.ReadLine());
+             Console.WriteLine("Введите значение столбцов n: ");
+             int n = Convert.ToInt32(Console.ReadLine());
+             string[,] massiv = new string[m, n];
+             for (int rows = 0; rows < m; rows++)
+             {
+                for (int columns = 0; columns < n; columns++)
+                {
+                  Console.Write($"{massiv[rows, columns]}0");
+                }
+             }
+               Console.WriteLine();
+                for (int i = 0; i < m; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        massiv[i,j] = Convert.ToString(new Random().Next(1,10));
+                        Console.Write($" {massiv[i,j]} ");
+                    }
+                    Console.WriteLine();
+                }
          }
      }  
 }   
