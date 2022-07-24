c1ee8d21aba2830125bfa4721d5bc49e18909e7f
«Aleksandr» <akhlopushin98@mail.ru>
2022-07-24 13:13:38 +0400

    Кое как решено

---
+++ ЗадачиСеминар8/Задача54/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит 
+// по возрастанию элементы каждой строки двумерного массива.
+namespace Zadacha54
+{
+   class Zadachi
+    {
+         static void Main(string[] args)
+        {
+         string[,] massiv = new string[2, 2];
+         string k;
+          for (int i = 0; i < 2; i++)
+            {
+              for (int j = 0; j < 2; j++)
+                {
+                  massiv[i,j] = Convert.ToString(new Random().Next(1,10));
+                  Console.Write($" {massiv[i,j]} ");
+                }
+              Console.WriteLine();
+            }
+          Console.WriteLine();
+           if (Convert.ToDouble(massiv[0,0]) > Convert.ToDouble(massiv[0,1]))
+           {
+             k = massiv[0,0];
+             massiv[0,0] = massiv[0,1];
+             massiv[0,1] = k;
+           }
+           Console.WriteLine($"{massiv[0,0]} {massiv[0,1]} ");
+           if (Convert.ToDouble(massiv[1,0]) > Convert.ToDouble(massiv[1,1]))
+           {
+             k = massiv[1,0];
+             massiv[1,0] = massiv[1,1];
+             massiv[1,1] = k;
+           }
+           Console.WriteLine($"{massiv[1,0]} {massiv[1,1]} ");
+         }
+        //  Я буду благодарен, если в ответ на ДЗ вы скинете задачу с лучшем реением чем у меня.
+        // Пытался решить ее 2 дня, к сожалению не вышло для общего случая. Но задача соответствует заданию))))
+     }  
+}   
