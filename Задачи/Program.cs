93528ba2d23b9197a890be6bc7dafe5c6789043c
«Aleksandr» <akhlopushin98@mail.ru>
2022-07-24 13:14:31 +0400

    21

--- Задачи/Program.cs
+++ Задачи/Program.cs
@@ -157,31 +157,37 @@
 
 // Перестановка чисел в массиве
 
-// int[] arr = { 1, 4, 8, 9, 1, 2, 4, 6, 3 };
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] arr = { 1, 4, 8, 9, 1, 2, 4, 6, 3 };
 
-// void PrintArray(int[] array)
-// {
-//     int count = array.Length;
-//     for (int i = 0; i < count; i++)
-//     {
-//         Console.Write($"{array[i]}");
-//     }
-// }
-// void SelectionSort(int[] array)
-// {
-//     for (int i = 0; i < array.Length - 1; i++)
-//     {
-//         int minPosition = i;
-//         for (int j = i + 1; j < array.Length; j++)
-//         {
-//            if(array[j] < array[minPosition]) minPosition = j; 
-//         }
-//         int temporary = array[i];
-//         array[i] = array[minPosition];
-//         array[minPosition] = temporary;
-//     }
-// }
-// PrintArray(arr);
-// SelectionSort(arr);
-
-// PrintArray(arr);
+        void PrintArray(int[] array)
+        {
+            int count = array.Length;
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write($"{array[i]}");
+            }
+        }
+        void SelectionSort(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                int minPosition = i;
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[j] < array[minPosition]) minPosition = j;
+                }
+                int temporary = array[i];
+                array[i] = array[minPosition];
+                array[minPosition] = temporary;
+            }
+        }
+        PrintArray(arr);
+        SelectionSort(arr);
+        Console.WriteLine();
+        PrintArray(arr);
+    }
+}
