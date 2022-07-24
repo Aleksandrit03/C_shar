3730dc0f17ddbca1f21d0a0ed32c51ec8eeea869
«Aleksandr» <akhlopushin98@mail.ru>
2022-07-24 17:45:29 +0400

    Добавил 58 задачу

---
+++ ЗадачиСеминар8/Задача58/Program.cs
@@ -0,0 +1,47 @@
+﻿//  Задайте две матрицы. Напишите программу, которая выведет матрицу 
+// произведения элементов двух предыдущих матриц.
+namespace Zadacha58
+{
+   class Zadachi
+    {
+         static void Main(string[] args)
+        {
+             string[,] massiv = new string[3, 3];
+             for (int i = 0; i < 3; i++)
+             {
+                for (int j = 0; j < 3; j++)
+                {
+                    massiv[i,j] = Convert.ToString(new Random().Next(1,10));
+                    Console.Write($" {massiv[i,j]} ");
+                }
+                Console.WriteLine();
+             }
+            Console.WriteLine();
+
+            string[,] mass = new string[3, 3];
+             for (int i = 0; i < 3; i++)
+             {
+                for (int j = 0; j < 3; j++)
+                {
+                    mass[i,j] = Convert.ToString(new Random().Next(1,10));
+                    Console.Write($" {mass[i,j]} ");
+                }
+                Console.WriteLine();
+             }
+            Console.WriteLine();
+            int[,] resultMass = new int [3, 3];
+            for (int i = 0; i < 3; i++)
+             {
+                for (int j = 0; j < 3; j++)
+                {
+                    resultMass[i,j] = Convert.ToInt32(massiv[i,j]) * Convert.ToInt32(mass[i,j]);
+                    Console.Write($" {resultMass[i,j]} ");
+                }
+                Console.WriteLine();
+             }
+            Console.WriteLine();
+            Console.ReadKey();
+         }
+     }  
+}   
+
