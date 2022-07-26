0bf87f391f52ce7d4bbc0fc8f08077654a92f481
«Aleksandr» <akhlopushin98@mail.ru>
2022-07-26 10:14:16 +0400

    Решен задачи семинара 9

---
+++ ЗадачиСеминар9/Задача64/Program.cs
@@ -0,0 +1,22 @@
+﻿//  Задайте значения M и N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от M до N.
+namespace Zadacha64
+{
+   class Zadachi
+    {
+         static void Main(string[] args)
+        {
+             Console.WriteLine("Введите значение M: ");
+             int M = Convert.ToInt32(Console.ReadLine());
+             Console.WriteLine("Введите значение N: ");
+             int N = Convert.ToInt32(Console.ReadLine());
+             Console.WriteLine($"M: {M} N: {N} ");
+             while(M <= N)
+             {
+               Console.Write($"{M} ");
+               M++;
+             }
+             Console.ReadKey();
+         }
+     }  
+}   
---
+++ ЗадачиСеминар9/Задача66/Program.cs
@@ -0,0 +1,24 @@
+﻿//  Задайте значения M и N. Напишите программу, которая найдёт 
+//  сумму натуральных элементов в промежутке от M до N.
+namespace Zadacha66
+{
+   class Zadachi
+    {
+         static void Main(string[] args)
+        {
+             Console.WriteLine("Введите значение M: ");
+             int M = Convert.ToInt32(Console.ReadLine());
+             Console.WriteLine("Введите значение N: ");
+             int N = Convert.ToInt32(Console.ReadLine());
+             Console.WriteLine($"M: {M} N: {N} ");
+             int result = 0;
+             while(M <= N)
+             {
+               result += M;
+               M++;
+             }
+             Console.Write($"{result}");
+             Console.ReadKey();
+         }
+     }  
+}   
